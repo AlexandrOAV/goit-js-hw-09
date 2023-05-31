dab7eb47c0af69ce87195befde79fdc4122a2ea2
AlexandrOAV <a0617011995@gmail.com>
2023-05-31 09:46:40 +0300

    task01-start-02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let bodyColorId = null;
+buttonStopEl.disabled = true;
+
+function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+function startColor() {
+        bodyColorId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+        buttonStartEl.disabled = true;
+        buttonStopEl.disabled = false;
+    }, 1000);
+};
+function stopColor() {
+    clearInterval(bodyColorId);
+    buttonStartEl.disabled = false;
+    buttonStopEl.disabled = true;
+};
+buttonStartEl.addEventListener('click',  startColor);
+buttonStopEl.addEventListener('click', stopColor);
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+require("flatpickr/dist/themes/dark.css")
+inputEl = document.getElementById('datetime-picker');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr(inputEl, options);
