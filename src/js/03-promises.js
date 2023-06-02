6bf409ed6e6d272775886c48784ccc6411e51ac2
AlexandrOAV <a0617011995@gmail.com>
2023-06-02 10:36:48 +0300

    task-03 end

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,96 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  width: '360px',
+  position: 'right-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
+  distance: '10px',
+  opacity: 1,
+  borderRadius: '5px',
+  rtl: false,
+  timeout: 4000,
+  messageMaxLength: 110,
+  backOverlay: false,
+  backOverlayColor: 'rgba(0,0,0,0.5)',
+  plainText: true,
+  showOnlyTheLastOne: false,
+  clickToClose: false,
+  pauseOnHover: true,
+
+  ID: 'NotiflixNotify',
+  className: 'notiflix-notify',
+  zindex: 4001,
+  fontFamily: 'Quicksand',
+  fontSize: '16px',
+  cssAnimation: true,
+  cssAnimationDuration: 400,
+  cssAnimationStyle: 'fade', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
+  closeButton: false,
+  useIcon: false,
+  useFontAwesome: false,
+  fontAwesomeIconStyle: 'basic', // 'basic' - 'shadow'
+  fontAwesomeIconSize: '34px',
+
+  success: {
+    background: '#32c682',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-success',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-check-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(50,198,130,0.2)',
+  },
+
+  failure: {
+    background: '#ff5549',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-failure',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-times-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(255,85,73,0.2)',
+  },
+});
+const formEl = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name=delay]');
+const inputStep = document.querySelector('input[name=step]');
+const inputAmount =document.querySelector('input[name=amount]');
+let delay= null;
+let step= null;
+let amount = null;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+       if (shouldResolve) {
+         resolve({ position, delay })
+         console.log(Promise)
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+};
+
+formEl.addEventListener('submit', addForm);
+function addForm(event) {
+  event.preventDefault();
+  delay = Number(inputDelay.value);
+  step = Number(inputStep.value);
+  amount = Number(inputAmount.value);
+  let position = 1;
+  for (let i = 0; i < amount; i++){
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
+    position += 1;
   }
 }
+
+
+ 
+
